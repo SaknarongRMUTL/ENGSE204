ac5c8a57602a6428bb4e6b0002963c8c06da75c5
SaknarongRMUTL <saknarong_2025@live.rmutl.ac.th>
2025-12-01 14:38:56 +0700

    Merge branch 'main' of https://github.com/SaknarongRMUTL/ENGSE204.git into main
    Signed-off-by: SaknarongRMUTL <saknarong_2025@live.rmutl.ac.th>

---
+++ LAB2/LAB2_2-src/LAB2_2/Main.java
@@ -0,0 +1,12 @@
+package LAB2_2;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		String id = input.nextLine();
+        String name = input.nextLine();
+        Student data = new Student(id, name);
+        data.displayinfo();
+        input.close();
+	}
+}
---
+++ LAB2/LAB2_2-src/LAB2_2/Student.java
@@ -0,0 +1,13 @@
+package LAB2_2;
+public class Student {
+	private String studentid;
+	private String name;
+	public Student(String studentid, String name) {
+		this.studentid = studentid;
+		this.name = name;
+	}
+	public void displayinfo() {
+		System.out.println(this.studentid);
+		System.out.println(this.name);
+	}
+}
